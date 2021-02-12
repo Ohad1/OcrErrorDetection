422af7a6d4022300d18bd90c6dfd87476f7e8517
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2021-02-12 17:49:29 +0200

    3

--- DictionaryCreator.py
+++ DictionaryCreator.py
@@ -5,8 +5,6 @@ from bs4 import BeautifulSoup
 import re
 import os
 
-inputDir = os.curdir
-
 
 def parseAct(xmlFile):
     content = []
@@ -19,36 +17,53 @@ def parseAct(xmlFile):
 
 
 # read משרד המשפטים xml
-texts = []
+
+
+def IsClause(word):
+    return re.match(r'\(M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})\)$', word.upper()) or \
+           re.match(r'\(\w\)$', word) or \
+           re.match(r'\(\D\d+\)$', word) or \
+           re.match(r'\(\d+\D\)$', word)
 
 
 def reSub(line):
     line = regex.sub(r'\p{Pd}', '-', line)
+    if 'CD' in line:
+        print()
+    if 'MCM' in line:
+        print()
     # line = re.sub(r'[^\w\s־-–־]', '', line)
-    line = re.sub(r'[^\"\w\s-]', '', line)
+    # line = re.sub(r'[^\".\w\s-]', '', line)
     words = line.split()
+    output = []
     for i in range(len(words)):
-        words[i] = re.sub(r'^[\"-]', '', words[i])
-        words[i] = re.sub(r'[\"-]$', '', words[i])
-    return words
+        # (re.match(r'\(\w+\)', )
+        if not IsClause(words[i]):
+            words[i] = re.sub(r'[^\".\w\s-]', '', words[i])
+            words[i] = re.sub(r'^[\"-.]', '', words[i])
+            words[i] = re.sub(r'[\"-.]$', '', words[i])
+            output.append(words[i])
+    output = list(filter(lambda a: a != '', output))
+    return output
 
 
-for currentpath, folders, files in os.walk(inputDir):
-    if len(texts) >= 10:
-        break
-    for file in files:
-        if file.endswith(".xml"):
-            print(currentpath)
-            content = parseAct(currentpath + "/" + file)
-            lines = []
-            for line in content:
-                words = reSub(line)
-                texts.append(words)
+def CreateDictionary(inputDir):
+    texts = []
+    for currentPath, folders, files in os.walk(inputDir):
+        # if len(texts) >= 10:
+        #     break
+        for file in files:
+            if file.endswith(".xml"):
+                content = parseAct(currentPath + "/" + file)
+                for line in content:
+                    words = reSub(line)
+                    texts.append(words)
+    all_words = set(itertools.chain.from_iterable(texts))
+    all_words.discard('')
+    return all_words
 
-
-all_words = list(itertools.chain.from_iterable(texts))
 
 # WORDS = Counter(itertools.chain.from_iterable(texts))
-print()
-
-
+# for w in all_words:
+#     print(w)
+# print(all_words)
--- Scanner.py
+++ Scanner.py
@@ -2,18 +2,75 @@ import zipfile
 import win32com.client
 import os
 import docx2txt
-# import textract
-
+from DictionaryCreator import *
+from nltk import bigrams
+import copy
+import json
 baseDir = 'C:\\Users\\Ohad\\PycharmProjects\\OcrErrorDetection\\PDF_DOC\\'  # Starting directory for directory walk
+
+
+def IsValid(word, dictionary):
+    return word in dictionary or re.match('^\d+$', word)
+
+
+def IsUndefinedNumber(num):
+    match = re.match('^[1-9]\d{4,5}$', num)
+    return match is not None
+
+
+def IsYear(year):
+    match = re.match('[1-3][0-9]{3}$', year)
+    return match is not None
+
+
+def IsAppendix(appendix):
+    match = re.match('^[1-9]\d{0,1}$', appendix)
+    return match is not None
+
 
 # filename = os.path.join(baseDir, "416226.DOCx")
 my_text = docx2txt.process(f"{baseDir}\\147859.DOCx")
-lines = my_text.split()
-
-for line in lines:
-    print(line)
-print(len(lines))
-
-# print(type(my_text))
+lines = my_text.split('\n\n')
 # print(my_text)
+docLines = []
+dictionary = CreateDictionary('akn')
+# with open('dict.json') as json_file:
+#     dictionary = json.load(json_file)
+# dictionary.add('1936')
+for line in lines:
+    # print(line)
+    words = reSub(line)
+    if words:
+        docLines.append(words)
+docWords = list(itertools.chain.from_iterable(docLines))
+print(len({w for w in docWords if w in dictionary}))
+print(len({w for w in docWords if w not in dictionary}))
+print(len(docWords))
 
+outputLines = copy.deepcopy(docLines)
+
+
+def IsLetter(docWord):
+    return re.match('^[a-z\u0590-\u05fe]$', docWord)
+
+
+for lineIndex, docLine in enumerate(docLines):
+    for wordIndex, docWord in enumerate(docLine):
+        if not IsValid(docWord, dictionary) or IsUndefinedNumber(docWord) or IsLetter(docWord):
+            outputLines[lineIndex][wordIndex] = f'<ש>{docWord}<ש>'
+    bg = list(bigrams(docLine))
+    for wordIndex, (w1, w2) in enumerate(bg):
+        if IsYear(w1) and IsAppendix(w2):
+            outputLines[lineIndex][wordIndex + 1] = f'<ש>{w2}<ש>'
+    print(outputLines[lineIndex])
+
+# for line in outputLines:
+#     x = " ".join(line.reverse())
+outputLines = [" ".join(line) for line in outputLines]
+with open('147859_Output.txt', 'w', encoding='utf-8') as outFile:
+    for line in outputLines:
+        outFile.write(f'{line}\n')
+# print(f'{w1} {w2}')
+
+# for w in docWords:
+#     print(w)

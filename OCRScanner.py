7227bc920da2e5e91fce7cd647ba45bf8542a549
shanisam <shanisam@post.bgu.ac.il>
2021-02-15 13:37:11 +0200

    build ocr scanner class and update scanner.

---
+++ OCRScanner.py
@@ -0,0 +1,52 @@
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
+from DictionaryCreator import *
+import os, sys
+
+
+def convert_pdf_to_image(pdf_path):
+    curr_dir_path = os.path.dirname(pdf_path)
+
+    # extract file + extension of file
+    base = os.path.basename(pdf_path)
+
+    # extract only file name - will be the name of folder
+    file_name = os.path.splitext(base)[0]
+    dir_name = f"{file_name}_img"
+    path_for_images = os.path.join(curr_dir_path, dir_name)
+
+    if not os.path.exists(dir_name):
+        # Create the directory
+        os.mkdir(path_for_images)
+    convert_from_path(pdf_path, output_folder=path_for_images)
+    return path_for_images
+
+# convert_pdf_to_image('/home/shanisam/OcrErrorDetection/pdf_for_ocr/146884.PDF')
+
+def change_extent_to_jpg(images_path):
+    for filename in os.listdir(images_path):
+        infilename = os.path.join(images_path,filename)
+        if not os.path.isfile(infilename): 
+            continue
+        oldbase = os.path.splitext(filename)
+        newname = infilename.replace('.ppm', '.jpg')
+        output = os.rename(infilename, newname)
+
+# change_extent_to_jpg('/home/shanisam/OcrErrorDetection/pdf_for_ocr/146884_img')
+
+def ocr_on_images(images_path):
+    ocr_texts = []
+    for filename in os.listdir(images_path):
+        infilename = os.path.join(images_path,filename)
+        if not os.path.isfile(infilename): 
+            continue
+        # using Pillow's Image class to open the image and pytesseract to detect the string in the image
+        text_from_pic = pytesseract.image_to_string(Image.open(infilename), lang='heb')
+        words = reSub(text_from_pic)
+        ocr_texts.append(words)
+    all_words = set(itertools.chain.from_iterable(ocr_texts))
+    all_words.discard('')
+    return all_words
+
+# ocr_on_images('/home/shanisam/OcrErrorDetection/pdf_for_ocr/146884_img')
--- Scanner.py
+++ Scanner.py
@@ -1,45 +1,61 @@
 import zipfile
-import win32com.client
+# import win32com.client
 import os
 import docx2txt
 from DictionaryCreator import *
+from OCRScanner import *
 from nltk import bigrams
 import copy
 import json
 
-baseDir = 'C:\\Users\\Ohad\\PycharmProjects\\OcrErrorDetection\\PDF_DOC\\'  # Starting directory for directory walk
-
+# ----------------------- HELP FUNCTIONS ------------------------
 
 def IsValid(word, dictionary):
     valid = lambda w: w in dictionary or re.match('^\d+$', w) or '"' in w or re.match('^[a-z\u0590-\u05fe]{1,2}\'$', w)
     return valid(word) or all(valid(w) for w in word.split('-'))
     # return word in dictionary or re.match('^\d+$', word) or '"' in word
 
-
 def IsUndefinedNumber(num):
     match = re.match('^[1-9]\d{4,5}$', num)
     return match is not None
-
 
 def IsYear(year):
     match = re.match('[1-3][0-9]{3}$', year)
     return match is not None
 
-
 def IsAppendix(appendix):
     match = re.match('^[1-9]\d{0,1}$', appendix)
     return match is not None
 
+def IsLetter(docWord):
+    return re.match('^[a-z\u0590-\u05fe]$', docWord)
 
+def create_dict_from_sec_ocr(pdf_path):
+    path_for_images = convert_pdf_to_image(pdf_path)
+    change_extent_to_jpg(path_for_images)
+    ocr_dict = ocr_on_images(path_for_images)
+    return ocr_dict
+
+# ----------------------- END HELP FUNCTIONS ------------------------
+
+baseDir = 'C:\\Users\\Ohad\\PycharmProjects\\OcrErrorDetection\\PDF_DOC\\'  # Starting directory for directory walk
+base_dir_shani = '/home/shanisam/OcrErrorDetection/pdf_for_ocr'
+
+docx_name = "146884.DOCx"
+pdf_name = "146884.PDF"
+# pdf_path = f"{baseDir}\\{pdf_name}"
+pdf_path = f"{base_dir_shani}/{pdf_name}"
+ocr_dict = create_dict_from_sec_ocr(pdf_path)
 # filename = os.path.join(baseDir, "416226.DOCx")
-my_text = docx2txt.process(f"{baseDir}\\147859.DOCx")
+# my_text = docx2txt.process(f"{baseDir}\\{docx_name}")
+my_text = docx2txt.process(f"{base_dir_shani}/{docx_name}")
 lines = my_text.split('\n\n')
 # print(my_text)
 docLines = []
-dictionary = CreateDictionary('akn')
-# with open('dict.json') as json_file:
-#     dictionary = json.load(json_file)
-# dictionary.add('1936')
+# dictionary = CreateDictionary('akn')
+with open('dict.json') as json_file:
+    dictionary = json.load(json_file)
+
 for line in lines:
     # print(line)
     words = reSub(line)
@@ -51,10 +67,6 @@ print(len({w for w in docWords if w not in dictionary}))
 print(len(docWords))
 
 outputLines = copy.deepcopy(docLines)
-
-
-def IsLetter(docWord):
-    return re.match('^[a-z\u0590-\u05fe]$', docWord)
 
 
 for lineIndex, docLine in enumerate(docLines):
